379fd643862fa519f7ccef17c3abb47eb7e3cec0
Paulo Henrique (Bode) <76111995+paulopradella@users.noreply.github.com>
2023-04-19 23:09:08 -0300

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -1,35 +1,28 @@
 #!/bin/sh
 
-# Get the token from Travis environment vars and build the bot URL:
+# Set the bot URL
 BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
 
-# Set formatting for the message. Can be either "Markdown" or "HTML"
+# Set the formatting for the message. Can be either "Markdown" or "HTML"
 PARSE_MODE="Markdown"
 
-# Use built-in Travis variables to check if all previous steps passed:
-if [ $TRAVIS_TEST_RESULT -ne 0 ]; then
-    build_status="failed"
+# Define the message text
+MESSAGE="*Travis CI build #${TRAVIS_BUILD_NUMBER} (${TRAVIS_BUILD_ID}) for ${TRAVIS_REPO_SLUG}*\n\n"
+
+# Add the commit message and author details
+MESSAGE+="*Commit Message*: ${TRAVIS_COMMIT_MESSAGE}\n"
+MESSAGE+="*Commit Author*: ${TRAVIS_COMMIT_AUTHOR}\n"
+MESSAGE+="*Commit Email*: ${TRAVIS_COMMIT_AUTHOR_EMAIL}\n\n"
+
+# Check if the build passed or failed and add the status to the message
+if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
+  MESSAGE+="*Status*: Build succeeded"
 else
-    build_status="succeeded"
+  MESSAGE+="*Status*: Build failed"
 fi
 
-# Define send message function. parse_mode can be changed to
-# HTML, depending on how you want to format your message:
-send_msg () {
-    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
-        -d text="$1" -d parse_mode=${PARSE_MODE}
-}
+# Add the URL to the build log to the message
+MESSAGE+="\n*Log*: ${TRAVIS_BUILD_WEB_URL}"
 
-# Send message to the bot with some pertinent details about the job
-# Note that for Markdown, you need to escape any backtick (inline-code)
-# characters, since they're reserved in bash
-send_msg "
--------------------------------------
-Travis build *${build_status}!*
-\`Repository:  ${TRAVIS_REPO_SLUG}\`
-\`Branch:      ${TRAVIS_BRANCH}\`
-*Commit Msg:*
-${TRAVIS_COMMIT_MESSAGE}
-[Job Log here](${TRAVIS_JOB_WEB_URL})
---------------------------------------
-"
+# Send the message to the bot
+curl -s -X POST ${BOT_URL} -d chat_id=${TELEGRAM_CHAT_ID} -d text="${MESSAGE}" -d parse_mode=${PARSE_MODE}
