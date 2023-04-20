8a6286d189db2027e699cc5b0298116def0ecedd
Paulo Henrique (Bode) <76111995+paulopradella@users.noreply.github.com>
2023-04-20 00:32:05 -0300

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -7,19 +7,32 @@ BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
 PARSE_MODE="Markdown"
 
 # Define the message text
-MESSAGE="*Travis CI build #${TRAVIS_BUILD_NUMBER} (${TRAVIS_BUILD_ID}) for ${TRAVIS_REPO_SLUG}*\n\n"
+MESSAGE=""
 
-# Add the commit message and author details
-MESSAGE+="*Commit Message*: ${TRAVIS_COMMIT_MESSAGE}\n"
-MESSAGE+="*Commit Author*: ${TRAVIS_COMMIT_AUTHOR}\n"
-MESSAGE+="*Commit Email*: ${TRAVIS_COMMIT_AUTHOR_EMAIL}\n\n"
-
-# Check if the build passed or failed and add the status to the message
-if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
-  MESSAGE+="*Status*: Build succeeded"
-else
-  MESSAGE+="*Status*: Build failed"
-fi
+# Check the Travis CI build phase
+case $TRAVIS_BUILD_STAGE_NAME in
+  "test-sast")
+    MESSAGE+="*Travis CI SAST Scan #${TRAVIS_BUILD_NUMBER} (${TRAVIS_BUILD_ID}) for ${TRAVIS_REPO_SLUG}*\n\n"
+    MESSAGE+="*Status*: SAST Scan completed"
+    ;;
+  "deploy")
+    MESSAGE+="*Travis CI Deployment #${TRAVIS_BUILD_NUMBER} (${TRAVIS_BUILD_ID}) for ${TRAVIS_REPO_SLUG}*\n\n"
+    MESSAGE+="*Status*: Deployment completed"
+    ;;
+  *)
+    MESSAGE+="*Travis CI Build #${TRAVIS_BUILD_NUMBER} (${TRAVIS_BUILD_ID}) for ${TRAVIS_REPO_SLUG}*\n\n"
+    # Add the commit message and author details
+    MESSAGE+="*Commit Message*: ${TRAVIS_COMMIT_MESSAGE}\n"
+    MESSAGE+="*Commit Author*: ${TRAVIS_COMMIT_AUTHOR}\n"
+    MESSAGE+="*Commit Email*: ${TRAVIS_COMMIT_AUTHOR_EMAIL}\n\n"
+    # Check if the build passed or failed and add the status to the message
+    if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
+      MESSAGE+="*Status*: Build succeeded"
+    else
+      MESSAGE+="*Status*: Build failed"
+    fi
+    ;;
+esac
 
 # Add the URL to the build log to the message
 MESSAGE+="\n*Log*: ${TRAVIS_BUILD_WEB_URL}"
